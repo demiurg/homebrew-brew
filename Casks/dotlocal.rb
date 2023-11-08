e11e2dd006483295b2b5d1af66390f981bac5c75
Degreat <mail@degreat.co.uk>
2023-11-08 06:41:39 +0000

    Add cask

---
+++ Casks/dotlocal.rb
@@ -0,0 +1,18 @@
+cask "dotlocal" do
+  version "0.0.1"
+  sha256 "bc5d196d629105ec423152fd46ed1d0aab4e03e996fb0a64584029300011fc54"
+
+  url "https://degreat.co.uk/packages/DotLocal-#{version}.zip"
+  name "DotLocal"
+  desc "Mirror your production domains on your .local machine"
+  homepage "https://degreat.co.uk/dotlocal"
+
+  app "DotLocal.app"
+
+  depends_on macos: ">= :sonoma"
+  depends_on formula: "blackmann/brew/dotlocalctl"
+
+  livecheck do
+    skip "No version information available"
+  end
+end
